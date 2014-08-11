5336c781809530555de7a0f59821d734eeddda24
Ovan Pulu <ovan89@gmail.com>
2014-08-11 12:39:29 +0700

    logout

--- logout.php
+++ logout.php
@@ -7,7 +7,7 @@ session_destroy();
 
 echo "<script>window.open('http://www.twitter.com/logout', 'testing', 'height=700,width=300')</script>";
 
-$url = 'http://localhost/nestle/nestle/';
-echo "<meta http-equiv=\"Refresh\" content=\"0; url={$url}\">";
+
+echo "<meta http-equiv=\"Refresh\" content=\"0; url=./\">";
 // echo "<script>window.location.href='./'</script>";
 ?>
