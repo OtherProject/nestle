39b39cbbdae5b4eb8367f189e2b694d8b29fcdeb
Ovan Pulu <ovan89@gmail.com>
2014-09-04 21:02:09 +0700

    revisi download twitter

--- shareTemplate.php
+++ shareTemplate.php
@@ -1,11 +1,19 @@
 <?php
 
 $foto = $_GET['ft'];
+$basedomain = "http://rumahnutrisinestle.com/gowithactivgro/";
+
+    if(count($_GET) == 2){
+
+	}else{
+		$redirect = $basedomain."public_assets/imageFramed/".$foto;
+		echo "<meta http-equiv=\"Refresh\" content=\"0; url=$redirect\">";
+	}
 ?>
 <meta property="og:title" content="Go With ActivGro" />
 <meta property="og:type" content="website" />
 
-<img width="100%" src="http://rumahnutrisinestle.com/gowithactivgro/public_assets/imageFramed/<?php echo $foto?>" class="bigImage1" id="profilePicture"/>
+<img width="100%" src="<?php echo $basedomain?>public_assets/imageFramed/<?php echo $foto?>" class="bigImage1" id="profilePicture"/>
 
 
 
